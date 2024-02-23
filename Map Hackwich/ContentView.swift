2a335190548f78cd7ffc836bd5d87e5161d36c46
Chase Hashiguchi <chashiguchi@bsd220.org>
2024-02-23 09:39:17 -0600

    Added geocoder lookup

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -24,15 +24,31 @@ struct ContentView: View {
                                         latitude: 42.15704, longitude: -88.14812))]
     var body: some View {
         Map(coordinateRegion: $region,
-                  interactionModes: .all,
-                  showsUserLocation: true,
-                  userTrackingMode: $userTrackingMode,
-                  annotationItems: places) { place in
-                  MapAnnotation(coordinate: place.coordinate,
-                                anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
-                      Marker(name: place.name)
-                  }
-              }
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+            }
+        }
+            .onAppear {
+                findLocation(name: "Springfield")
+            }
+    }
+    func findLocation(name: String) {
+        locationManager.geocoder.geocodeAddressString(name) { (placemarks, error) in
+            guard placemarks != nil else {
+                print("Could not locate \(name)")
+                return
+            }
+            for placemark in placemarks! {
+                let place = Place(name: "\(placemark.name!), \(placemark.administrativeArea!)",
+                                  coordinate: placemark.location!.coordinate)
+                places.append(place)
+            }
+        }
     }
 }
 
